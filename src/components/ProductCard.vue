ebdb57738f595395da7f725ce392716befb39d02
tugce23 <tugceergur191@gmail.com>
2026-04-10 18:13:10 +0300

    feat: improve product card UI and add dynamic pricing with discount calculation

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,30 +1,59 @@
 <template>
-  <v-card>
+<v-card class="product-card" elevation="0">
+
+  <!-- IMAGE -->
+  <div class="image-wrapper">
     <v-img
       :src="product.image"
-      height="200"
+      height="240"
       cover
     />
 
-    <v-card-title>
+    <!-- badge -->
+    <div class="badge">Yeni</div>
+  </div>
+
+  <!-- CONTENT -->
+  <v-card-text class="pa-3">
+
+    <div class="title">
       {{ product.title }}
-    </v-card-title>
+    </div>
 
-    <v-card-text>
-      {{ product.price }} ₺
-    </v-card-text>
+    <div class="price-row">
+  
+  <!-- sadece varsa göster -->
+  <span v-if="product.oldPrice" class="old-price">
+    {{ product.oldPrice }} ₺
+  </span>
 
-    <v-card-actions>
-      <v-btn @click="cart.addToCart(product)">
-  Add to Cart
-</v-btn>
-    </v-card-actions>
-  </v-card>
+  <span class="price">
+    {{ product.price }} ₺
+  </span>
+
+</div>
+<div v-if="product.oldPrice" class="discount-badge">
+  %{{ discountPercent }}
+</div>
+    <v-btn
+      block
+      color="black"
+      class="mt-2 add-btn"
+      @click="addProduct(product)"
+    >
+      Sepete Ekle
+    </v-btn>
+<v-snackbar v-model="snackbar">
+  Ürün sepete eklendi 🛒
+</v-snackbar>
+  </v-card-text>
+
+</v-card>
 </template>
 
 <script setup lang="ts">
 import type { Product } from '@/types/Product'
-
+import { ref ,computed} from 'vue'
 import { useCartStore } from '@/stores/cart.store'
 const cart = useCartStore()
 
@@ -33,4 +62,80 @@ const props = defineProps<{
   product: Product
 }>()
 
+const snackbar = ref(false)
+
+const addProduct = (product) => {
+  cart.addToCart(product)
+  snackbar.value = true
+}
+
+
+const discountPercent = computed(() => {
+  if (!props.product?.oldPrice) return 0
+
+  return Math.round(
+    ((props.product.oldPrice - props.product.price) / props.product.oldPrice) * 100
+  )
+})
+
 </script>
+<style lang="css">
+.product-card {
+  border-radius: 12px;
+  overflow: hidden;
+  transition: 0.2s ease;
+}
+
+.product-card:hover {
+  box-shadow: 0 8px 20px rgba(0,0,0,0.08);
+}
+
+/* image */
+.image-wrapper {
+  position: relative;
+}
+
+/* badge */
+.badge {
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  background: black;
+  color: white;
+  font-size: 11px;
+  padding: 4px 8px;
+  border-radius: 6px;
+}
+
+/* title */
+.title {
+  font-size: 14px;
+  font-weight: 500;
+  height: 40px;
+  overflow: hidden;
+}
+
+/* price */
+.price {
+  font-size: 18px;
+  font-weight: 700;
+}
+
+/* button */
+.add-btn {
+  border-radius: 8px;
+  text-transform: none;
+}
+
+.discount-badge {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  background: red;
+  color: white;
+  font-size: 12px;
+  padding: 4px 6px;
+  border-radius: 6px;
+}
+
+</style>
