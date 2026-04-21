136081ccac63c3de78151877159eeb4b6857b2f5
tugce23 <tugceergur191@gmail.com>
2026-04-21 18:25:21 +0300

    feat: add register page

---
+++ src/pages/Login.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+    <h2>Login</h2>
+    <input v-model="email" placeholder="Email"/>
+      <input v-model="password" placeholder="Password"/>
+      <button @click="login">Login</button>
+    </div>
+</template>
+<script lang="ts" setup>
+import {ref} from 'vue'
+import { useAuthStore } from '../stores/auth.store';
+
+const authStore=useAuthStore()
+const email=ref('')
+const password=ref('')
+
+const login=()=>{
+    authStore.login(email.value,password.value)
+}
+
+</script>
---
+++ src/pages/Register.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+<h2>Register</h2>
+<input v-model="email" placeholder="Email"/>
+<input v-model="password" type="password" placeholder="Password"/>
+<button @click="register">Register</button>
+</div>
+
+</template>
+<script lang="ts" setup>
+import {ref} from 'vue'
+import { useAuthStore } from '../stores/auth.store';
+
+const authStore=useAuthStore()
+const email=ref('')
+const password=ref('')
+
+const register=()=>{
+    authStore.register(email.value,password.value)}
+</script>
