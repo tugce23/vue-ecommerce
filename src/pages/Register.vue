83240b3c14fc3a146d627818af8cf5cf4bbbf4f8
tugce23 <tugceergur191@gmail.com>
2026-04-24 14:48:39 +0300

    style: edit register page

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -1,12 +1,28 @@
 <template>
-<div>
-<h2>Register</h2>
-<input v-model="email" placeholder="Email"/>
-<input v-model="password" type="password" placeholder="Password"/>
-<button @click="register">Register</button>
-</div>
+  <v-container   class="d-flex align-center justify-center"
+    style="height: 100vh;">
+<v-card class="mx-auto px-6 py-8" max-width="500">
+<v-card-title>Kayıt Ol</v-card-title>
+  <v-sheet class="mx-auto" width="400">
+    <v-form @submit.prevent="register">
+      <v-text-field
+        v-model="email"
+       variant="outlined"
+        label="Email"
+      ></v-text-field>
+      <v-text-field
+       v-model="password"
+       variant="outlined"
+        label="Password"
+      ></v-text-field>
+      <v-btn class="mt-2" size="large"  color="purple" type="submit" block>Register</v-btn>
+    </v-form>
+  </v-sheet>
+</v-card>
+  </v-container>
 
 </template>
+
 <script lang="ts" setup>
 import {ref} from 'vue'
 import { useAuthStore } from '../stores/auth.store';
