119a0f0a91d22900709b76a72aa143f8acf38017
tugce23 <tugceergur191@gmail.com>
2026-04-27 14:34:11 +0300

    feat: add navbar buttons

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,6 +1,13 @@
 <template>
   <v-app-bar color="primary" elevation="1">
     <v-app-bar-title>Vue E-Commerce</v-app-bar-title>
+     <v-btn variant="text" @click="goToLogin">
+      Login
+    </v-btn>
+
+    <v-btn variant="text" @click="goToRegister">
+      Register
+    </v-btn>
     <v-btn icon to="/cart">
   <v-badge
     :content="cartCount"
@@ -20,10 +27,22 @@
 <script setup lang="ts">
 import { computed } from 'vue'
 import { useCartStore } from '@/stores/cart'
-
+import { useRouter } from 'vue-router'
 const cartStore = useCartStore()
 
 const cartCount = computed(() =>
   cartStore.items.reduce((total, item) => total + item.quantity, 0)
 )
+
+
+
+const router = useRouter()
+
+const goToLogin = () => {
+  router.push('/login')
+}
+
+const goToRegister = () => {
+  router.push('/register')
+}
 </script>
