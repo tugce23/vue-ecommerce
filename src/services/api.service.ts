4aa894f727d09d470a045a60f902af6afa4738db
tugce23 <tugceergur191@gmail.com>
2026-05-06 17:46:38 +0300

    feat:token added

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,7 +1,19 @@
-import axios from "axios";
+
+import axios from 'axios'
+import { useAuthStore } from '@/stores/auth.store'
 
 const api = axios.create({
-  baseURL: "http://localhost:5095/api",
-});
+  baseURL: 'http://localhost:5095/api'
+})
 
-export default api;
+api.interceptors.request.use(config => {
+  const auth = useAuthStore()
+
+  if (auth.token) {
+    config.headers.Authorization = `Bearer ${auth.token}`
+  }
+
+  return config
+})
+
+export default api
--- src/stores/auth.store.ts
+++ src/stores/auth.store.ts
@@ -1,36 +1,47 @@
-import {defineStore} from 'pinia'
+import { defineStore } from 'pinia'
+import api from '@/services/api.service'
 
-export const useAuthStore =defineStore('auth',{
-    state:()=>({
-        user:null as null | {email:string},
-        token:null as string |null
+export const useAuthStore = defineStore('auth', {
+  state: () => ({
+    user: null as null | { email: string },
+    token: null as string | null
+  }),
 
-    }),
-    actions:{
-        login(email:string, password:string){
-            if(email && password){
-                this.user={email}
-                this.token='fake-jwt-token'
-                localStorage.setItem('token',this.token)
-                localStorage.setItem('user', JSON.stringify(this.user))
-            }
-        },
-        register(email:string, password:string){
-            console.log('registered',email,password)
-        },
-        logout(){
-            this.user=null,
-            this.token=null,
-            localStorage.removeItem('token')
-            localStorage.removeItem('user')
-        },
-        loadUser(){
-            const token=localStorage.getItem('token')
-            const user=localStorage.getItem('user')
-            if(token && user){
-                this.token=token,
-                this.user=JSON.parse(user)
-            }
-        }
+  actions: {
+    async login(email: string, password: string) {
+      try {
+        const res = await api.post('/auth/login', {
+          email,
+          password
+        })
+
+        this.token = res.data.token
+        this.user = { email }
+
+        localStorage.setItem('token', this.token)
+        localStorage.setItem('user', JSON.stringify(this.user))
+
+      } catch (err) {
+        console.error('Login error', err)
+      }
+    },
+
+    logout() {
+      this.user = null
+      this.token = null
+
+      localStorage.removeItem('token')
+      localStorage.removeItem('user')
+    },
+
+    loadUser() {
+      const token = localStorage.getItem('token')
+      const user = localStorage.getItem('user')
+
+      if (token && user) {
+        this.token = token
+        this.user = JSON.parse(user)
+      }
     }
+  }
 })
